212c54e7aa28032fff05b0e2ffc2be35a18e011d
Raferna <raferna.dg@gmail.com>
2022-08-13 19:43:33 -0300

    V1

--- src/App.js
+++ src/App.js
@@ -5,19 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1><img src={logo} className="App-logo" alt="logo"/>Aprendiendo React<img src={logo} className="App-logo" alt="logo"/></h1>
       </header>
+
+      <>
+        <h3>Desafío Clase 1</h3>
+        <p>Crear la App utilizando el CLI</p>
+      </>
     </div>
   );
 }
